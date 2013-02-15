1ef0b82377336ae2e43018fb921ea98148486e53
Ben Pollock <B.O.Pollock@gmail.com>
2013-02-15 14:49:19 -0500

    no message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alooksie::Application.config.secret_token = '3f35bc9746f927fdeae91742cbd152dd1df0a6bd6ede32128ebc9bd3323cc38a356d0f264d133ab789b0e15ae48ca09e31797c2ab02f72c7ae82269a6b86e3a6'
