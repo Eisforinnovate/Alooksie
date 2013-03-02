f2068d63295fdd13bde0d95104ba3e8071a2c204
Ben Pollock <B.O.Pollock@gmail.com>
2013-03-02 13:58:52 -0500

    Fix Message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ActiveRecord::Base
   attr_accessible :content
   belongs_to :user
+  has_many :notifications
+  has_many :messagematches #might need to be messagematchs
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 200 }
   default_scope order: 'messages.created_at DESC'  #defaults to displaying recent first
