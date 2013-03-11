3c2b97a1ac3b9e7df268def87189a3f09a8f56e5
Mark Oberemk <me@moberemk.com>
2013-03-10 22:33:54 -0400

    Trying to determine just how much info Facebook hands over

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,13 @@ class SessionsController < ApplicationController
 		@authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
 		if @authorization
 			user = User.find_by_id(@authorization.user_id)
-			render :text => "Welcome back #{@authorization.user.name}! You have already signed up. <ul><li>ID: #{user.id}</li><li>Email: #{user.email}</li><li>Created: #{user.created_at}</li></ul>"
+			render :text => "Welcome back #{@authorization.user.name}! You have already signed up."
+			render user.to_json
 		else
 			user = User.new :name => auth_hash["info"]["name"], :email => auth_hash["info"]["email"]
 			user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
 			user.save
-			render :text => "Hi #{user.name}! You've signed up. <ul><li>ID: #{user.id}</li><li>Email: #{user.email}</li><li>Created: #{user.created_at}</li></ul>"
+			render :text => "Hi #{user.name}! You've signed up."
 		end
 	end
 
