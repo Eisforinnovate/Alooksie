5e38c6f417d449503be6b4472cbed6e08c88a8c4
Mark Oberemk <me@moberemk.com>
2013-03-10 22:16:21 -0400

    Testing user creation and signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,13 @@ class SessionsController < ApplicationController
 		#Check if there's an existing authorization
 		@authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
 		if @authorization
-			render :text => "Welcome back #{@authorization.user.name}! You have already signed up."
+			user = User.find_by_id(@authorization.user_id)
+			render :text => "Welcome back #{@authorization.user.name}! You have already signed up. <ul><li>ID: #{user.id}</li><li>Email: #{user.email}</li><li>Created: #{user.created_at}</li></ul>"
 		else
 			user = User.new :name => auth_hash["user_info"]["name"], :email => auth_hash["user_info"]["email"]
 			user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
 			user.save
-			render :text => "Hi #{user.name}! You've signed up."
+			render :text => "Hi #{user.name}! You've signed up. <ul><li>ID: #{user.id}</li><li>Email: #{user.email}</li><li>Created: #{user.created_at}</li></ul>"
 		end
 	end
 
@@ -22,5 +23,6 @@ class SessionsController < ApplicationController
 
 	def failure
 		flash[:notice] = "You've denied login access to our app!"
+	    redirect_to root_url
 	end
 end
