9443c13df43af04be7d8fcef601e84ff20b6b431
Mark Oberemk <me@moberemk.com>
2013-06-19 18:17:49 -0400

    Added a timeout function to the session controler

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,34 @@
 # encoding: UTF-8
 class SessionsController < ApplicationController
+	# Delete inactive sessions; default to one hour session expiry
+	def self.sweep(time = 1.hour)
+		if time.is_a?(String)
+			time = time.split.inject { |count unit| count.to_i.send(unit) }
+		end
+
+		delete_all "updated_at < '#{time.ago.to_s(:db)}'"
+	end
+
 	def create
 		auth_hash = request.env['omniauth.auth']
 		#Check if there's an existing authorization
 		@authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
 		if @authorization
 			user = User.find_by_id(@authorization.user_id)
-			render :text => "Welcome back #{@authorization.user.name}! You have already signed up. <ul><li>ID: #{user.id}</li><li>Email: #{user.email}</li><li>Created: #{user.created_at}</li><li>#{auth_hash.to_json}</li></ul>"
+			#render :text => "Welcome back #{@authorization.user.name}! You have already signed up. <ul><li>ID: #{user.id}</li><li>Email: #{user.email}</li><li>Created: #{user.created_at}</li><li>#{auth_hash.to_json}</li></ul>"
 		else
 			user = User.new :name => auth_hash["info"]["name"], :email => auth_hash["info"]["email"]
 			user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
 			user.save
-			render :text => "Hi #{user.name}! You've signed up. <ul><li>ID: #{user.id}</li><li>Email: #{user.email}</li><li>Created: #{user.created_at}</li></ul>"
+			#render :text => "Hi #{user.name}! You've signed up. <ul><li>ID: #{user.id}</li><li>Email: #{user.email}</li><li>Created: #{user.created_at}</li></ul>"
 		end
+
+		# Create a new user session
+		session[:user_id] = user.id
+		session[:user] = user
+
+		# Redirect to the user page
+		redirect_to :back
 	end
 
 	def destroy
