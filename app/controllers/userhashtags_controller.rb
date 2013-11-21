6078da86a13219d986490b271907370df6345206
Ben Pollock <B.O.Pollock@gmail.com>
2013-11-21 12:05:16 -0500

    Getting hashtag names but they are not getting rendered in json

--- app/controllers/userhashtags_controller.rb
+++ app/controllers/userhashtags_controller.rb
@@ -4,9 +4,15 @@ class UserhashtagsController < ApplicationController
 	def index
 		logger.debug "Trying to get user_hashtags"
 		if session[:user] && session[:user].id
-			@hashtags = Userhashtag.find(:all, conditions:["user_id = ?",session[:user].id])
-			
-			render :json => {:userhashtags => @hashtags}.to_json()
+			@uhashtags = Userhashtag.find(:all, conditions:["user_id = ?",session[:user].id])
+
+			@uhashtags.each do |hashtag|
+				h = Hashtag.find(hashtag.hashtag_id)
+				logger.debug "Our hashtags are: #{h.name}"
+				hashtag.name = h
+			end
+
+			render :json => {:userhashtags => @uhashtags}.to_json()
 		else
 			render :json => {}
 		end
