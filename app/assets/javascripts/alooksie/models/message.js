79735e4b4998a56596e6beca5ec689d929ad3417
Ben Pollock <B.O.Pollock@gmail.com>
2013-11-23 14:52:30 -0500

    Removed double priority call

--- app/assets/javascripts/alooksie/models/message.js
+++ app/assets/javascripts/alooksie/models/message.js
@@ -7,24 +7,4 @@ App.Message = DS.Model.extend({
 	updated_at: DS.attr('string'),
 	user: DS.belongsTo('user', {async: true}),
 	messagehashtag: DS.hasMany('messagehashtag'),
-<<<<<<< HEAD
-	priority: DS.attr('number'),
-	priorityClass: 'importance-3',
-=======
->>>>>>> b08e28f0ffb13854a2e885d13c441433314ca788
-	/**
-	 * Should return the curent message priority here
-	 * @return {[type]} [description]
-	 */
-	priorityClass: function(user_id){
-		if(this.priority == 1 || this.priority == 2)
-			return '';
-		else
-<<<<<<< HEAD
-			return 5;*/
-		return 'importance-'+Math.floor((Math.random()*5)+1);
-=======
-			return 'importance-medium';
->>>>>>> b08e28f0ffb13854a2e885d13c441433314ca788
-	}.property('priority')
 });
